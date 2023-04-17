cf38893d2e11eb602f59062858e58190e4b85106
BigUserName <cantfindaname37@gmail.com>
2023-04-17 10:19:06 -0400

    Ship displays on screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,15 +1,37 @@
-'''
-Alien Invasion Project from Python Crash Course
-Chapters 12 - 14
-Page 227 of the book.agee 265 of the PDF
-'''
-
 import sys
 import pygame
+from setting import Settings
+from ship import Ship
 
+class AlienInvasion:
+    def __init__(self):
+
+        pygame.init()
+        
+
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        
+        self.ship = Ship(self)
+        
+        pygame.display.set_caption('Alein Invasion')
+
+
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+            
+            pygame.display.flip()
 
 
 
 if __name__ == '__main__':
-    #run the game
-    pass
+    ai = AlienInvasion()
+    ai.run_game()
