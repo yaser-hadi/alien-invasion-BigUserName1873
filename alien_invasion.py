5e9aa7b74b2bf1ebdec8da20514a2cfcd2bc731c
BigUserName <cantfindaname37@gmail.com>
2023-04-19 10:20:16 -0400

    bullets now fire

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,18 +2,16 @@ import sys
 import pygame
 from setting import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     def __init__(self):
 
         pygame.init()
-        
-
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
-        
         self.ship = Ship(self)
-        
+        self.bullets = pygame.sprite.Group()
         pygame.display.set_caption('Alien Invasion')
 
 
@@ -22,6 +20,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
 
@@ -41,6 +40,8 @@ class AlienInvasion:
     def _update_screen(self):
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
+            for bullet in self.bullets.sprites():
+                bullet.draw_bullet()
             pygame.display.flip()
 
 
@@ -51,6 +52,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         if event.key == pygame.K_q:
             sys.exit()
+        if event.key == pygame.K_SPACE:
+            self._fire_bullet()
     
     
     def _check_keyup_events(self, event):
@@ -60,6 +63,20 @@ class AlienInvasion:
             self.ship.moving_left = False
 
 
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
+
+
+
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
+
+
+
+'''Notes so I don't forget 
+    Add shooting sfx
+    Add screaming when JFK dies
+    Change bullet to real bullet'''
--- setting.py
+++ setting.py
@@ -5,4 +5,12 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (0, 211, 0)
+        
+
         self.ship_speed = 0.5
+
+
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
