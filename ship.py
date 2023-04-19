8d3106d9c742193464d18b765f396da2ac0826b8
BigUserName <cantfindaname37@gmail.com>
2023-04-19 09:15:21 -0400

    changed ship

--- ship.py
+++ ship.py
@@ -5,7 +5,7 @@ class Ship:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
-        self.image = pygame.image.load('images/jfk.jpeg')
+        self.image = pygame.image.load('images/tiny_lee.jpg')
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = self.screen_rect.midbottom
